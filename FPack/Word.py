34ec5d0d5607d68a405ca598f69f11b47237d2d5
Maksim Viarbitski <MViarbitski@ibagroup.eu>
2023-02-04 11:52:25 +0300

    init

---
+++ FPack/Word.py
@@ -0,0 +1,10 @@
+'''
+Created on 4 февр. 2023 г.
+
+@author: Viarbitski_M
+'''
+
+if __name__ == '__main__':
+    message = ['Hello', 'world', '!']
+    text1, text2, text3 = message
+    print(text1, text2 + text3)
